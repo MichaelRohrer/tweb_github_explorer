cc6ca06bb83af7f40d8d0538e882efeb1b16a663
Michael Rohrer <Frederic@Yoda.local>
2016-10-02 11:54:31 +0200

    Ah

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/assets'));
-
 app.use("/assets", express.static(__dirname + '/assets'));
-//app.use('/static', express.static(__dirname + '/assets'));
+
+//app.use(express.static(__dirname + '/app'));
+
+app.use("/app", express.static(__dirname + '/app'));
+
+app.use("/app/modules", express.static(__dirname + '/app/modules'));
+
 
 app.use("/images", express.static(__dirname + '/images'));
 
@@ -17,5 +22,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
