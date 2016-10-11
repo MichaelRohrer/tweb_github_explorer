16d641af0ca9a2d4227520b1559af3dfe82990b4
Michael Rohrer <Frederic@Yoda.local>
2016-10-11 21:25:55 +0200

    GitHubApi Handeled

--- app/modules/page2/page2Ctrl.js
+++ app/modules/page2/page2Ctrl.js
@@ -13,20 +13,55 @@
 		.module('page2')
 		.controller('Page2Ctrl', Page2);
 
-		Page2.$inject = [];
+		function Page2($scope, $http) {
 
-		/*
-		* recommend
-		* Using function declarations
-		* and bindable members up top.
-		*/
+            var vm = this;
 
-		function Page2() {
-			/*jshint validthis: true */
-			var vm = this;
+			vm.owner = "MichaelRohrer";
+			vm.repo = "tweb_github_explorer";
 
-			vm.data = [54, 21, 9];
-			vm.labels = ["apple", "Pear", "Bananas"];
+            callRepoStatAPI(vm, $http);
 		}
 
+		function callRepoStatAPI($scope, $http) {
+
+			var url = "https://api.github.com";
+			var repos = "/repos";
+			//var owner = '/' + $scope.owner;
+			//var repo = '/' + $scope.repo;
+
+			var owner = '/' + "MichaelRohrer";
+			var repo = '/' + "tweb_github_explorer";
+
+			var stats = "/stats";
+			var contributors = "/contributors";
+
+			var fullurl = url + repos + owner + repo + stats + contributors;
+
+
+			//Get contributors list with additions, deletions, and commit counts
+			$http.get(fullurl)
+				.then(function(response) {
+
+					$scope.data = response.data;
+                    formatToDisplayableData($scope);
+
+				});
+		}
+
+		function formatToDisplayableData($scope){
+
+
+		    var obj = angular.fromJson($scope.data);
+
+            var data = [];
+            var label = [];
+
+            for(var i = 0; i < obj.length; ++i){
+                label.push(obj[i].author.login);
+                data.push(obj[i].total);
+            }
+            $scope.labels = label;
+            $scope.data = data;
+        }
 })();
