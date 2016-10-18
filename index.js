1522cd8392749eff49c5f5c3c8d85cd5bb7988af
Michael Rohrer <michael.rohrer@hotmail.ch>
2016-10-18 11:18:49 +0200

    BodyParsers

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
-app.use(express.json());       // to support JSON-encoded bodies
-app.use(express.urlencoded()); // to support URL-encoded bodies
 
 // assuming POST: name=foo&color=red            <-- URL encoding
 //
